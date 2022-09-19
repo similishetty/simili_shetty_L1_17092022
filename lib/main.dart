372ecfac051314f3323e0da3557ff75930d0c25d
Simili Shetty <similishetty1997@gmail.com>
2022-09-19 19:50:42 +0530

    Update Launcher Icons

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Movie Ticket Booking',
       theme: ThemeData(
         // This is the theme of your application.
         //
